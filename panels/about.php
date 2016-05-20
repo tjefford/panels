440de5ab03b95fc3298231a0f016839bdec7bc3e
Tyler Jefford <tjefford@me.com>
2016-05-19 20:59:42 -0500

    Update copy on about panel

--- panels/about.php
+++ panels/about.php
@@ -7,7 +7,6 @@ defined( 'SitePath' ) or die( 'No Direct Access Allowed' ); ?>
         <br />
         <p>If you have ever had the issue of running a website that you constantly want to update but feel like you never have enough time. Panels might be for you. With minimal setup, you will be off an running with your new single page website.</p>
         <p>With content being separated from the rest of the site, you can easily add new panels with unique content as well as extend the website because its just basic PHP.</p>
-        <p>Read more about the concept and set up on my <a href="#" target="_blank">Medium post</a> about Panels.</p>
         <br />
         <p>Demo Site Color Palette by <a href="http://www.colourlovers.com/palette/4217449/Petals">KarenH</a> on Colour Lovers</p>
     </div>
